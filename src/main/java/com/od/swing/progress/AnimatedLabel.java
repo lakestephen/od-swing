7db3fb71814ca5e3a314a8ee3657c88a96c4c690
Nick Ebbutt <nick@objectdefinitions.com>
2012-10-15 21:50:06 +0100

    Update for od-swing 1.0.13 release

--- src/main/java/com/od/swing/progress/AnimatedLabel.java
+++ src/main/java/com/od/swing/progress/AnimatedLabel.java
@@ -1,20 +1,20 @@
 /**
- * Copyright (C) 2009 (nick @ objectdefinitions.com)
+ * Copyright (C) 2012 (nick @ objectdefinitions.com)
  *
- * This file is part of JTimeseries.
+ * This file is part of Object Definitions od-swing.
  *
- * JTimeseries is free software: you can redistribute it and/or modify
+ * od-swing is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * JTimeseries is distributed in the hope that it will be useful,
+ * od-swing is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with JTimeseries.  If not, see <http://www.gnu.org/licenses/>.
+ * along with od-swing.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.od.swing.progress;
 
